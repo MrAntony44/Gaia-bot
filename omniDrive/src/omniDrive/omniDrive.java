5adac634d30935035c6168bb154f17a5dd581499
Gabriel Ioannidis <gabkion@gmail.com>
2020-05-06 21:38:31 +0300

    only omniDrive

--- omniDrive/src/omniDrive/omniDrive.java
+++ omniDrive/src/omniDrive/omniDrive.java
@@ -36,20 +36,7 @@ public class DriveJava extends LinearOpMode {
     private DcMotor back_left_wheel = null;
     private DcMotor back_right_wheel = null;
     private DcMotor front_right_wheel = null;
-    private DcMotor shooter_thing = null;
-    private DcMotor collection_thing = null;
-    private DcMotor lift_thing = null;
-    private DcMotor heavy = null;
-    private VuforiaFG2019 vuforia2019;
-    Servo servo;
-    Servo collection_servo1;
-    Servo collection_servo2;
-    Servo lift_servo1;
-    Servo lift_servo2;
-    Servo angle_servo;
-    
-       // VuforiaBase.TrackingResults vuforiaResul
-    
+
     BNO055IMU imu;
     @Override
     public void runOpMode()  {
@@ -59,26 +46,12 @@ public class DriveJava extends LinearOpMode {
         back_left_wheel = hardwareMap.dcMotor.get("back_left_wheel");
         back_right_wheel = hardwareMap.dcMotor.get("back_right_wheel");
         front_right_wheel = hardwareMap.dcMotor.get("front_right_wheel");
-        collection_thing = hardwareMap.dcMotor.get( "collection_thing");
-        
-        servo = hardwareMap.get(Servo.class , "servo");
-        collection_servo1 = hardwareMap.get(Servo.class , "collection_servo1");
-        collection_servo2 = hardwareMap.get(Servo.class , "collection_servo2");
-        lift_servo2 = hardwareMap.get(Servo.class , "lift_servo2");
-        lift_servo1 = hardwareMap.get(Servo.class , "lift_servo1");
-        angle_servo = hardwareMap.get(Servo.class , "angle_servo");
 
 
-        lift_thing = hardwareMap.get(DcMotorEx.class ,"lift_thing");
-        heavy = hardwareMap.get(DcMotorEx.class, "heavy");
-        shooter_thing = hardwareMap.dcMotor.get( "shooter_thing");
         front_left_wheel.setDirection(DcMotor.Direction.REVERSE); 
         back_left_wheel.setDirection(DcMotor.Direction.REVERSE);
         front_right_wheel.setDirection(DcMotor.Direction.FORWARD);
         back_right_wheel.setDirection(DcMotor.Direction.FORWARD);
-        collection_thing.setDirection(DcMotor.Direction.FORWARD);
-        lift_servo1.setDirection(Servo.Direction.REVERSE);
-        collection_servo1.setDirection(Servo.Direction.REVERSE);
 
        // vuforiaFG2019 = new Vuforia2019();
         front_left_wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -86,23 +59,11 @@ public class DriveJava extends LinearOpMode {
         front_right_wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         back_right_wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        vuforiaFG2019.initialize(hardwareMap.get(WebcamName.class, "Webcam 1"),
-        false, true, 0, 0, 0, -90, 180, 0);
+       
         imu = hardwareMap.get(BNO055IMU.class, "imu");
         //lift_thing.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-        lift_thing.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        heavy.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        lift_thing.setTargetPosition(0);
-        heavy.setTargetPosition(0);
-
         //lift_thing.setVelocity(300);
-        heavy.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        lift_thing.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        parameters.mode                = BNO055IMU.SensorMode.IMU;
-        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
-        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-        parameters.loggingEnabled      = false;
         imu.initialize(parameters);
       //  vuforiaFG2019.initialize(hardware.get(WebcamNameWebcamName.class, "Webcam 1"),false,true,0,0,0,-90,180,0);
         while(!opModeIsActive()){}
@@ -112,72 +73,9 @@ public class DriveJava extends LinearOpMode {
             resetAngle();
             vuforiaFG2019.activate();
             //driveSimple();
-            
             telemetry.update();
-
-            
-            
-        // Get the data for the Vuforia target
-        vuforiaResult = vuforiaFG2019.track("World_Map");
-        // Print the target's data to telemetry
-        printVuforiaTelemetry();
         }
-      // Deactivate Vuforia when the loop finishes
-      vuforiaFG2019.deactivate();
         
-    }
-    private void printVuforiaTelemetry() {
-    telemetry.addData("World Map Target", "");
-    telemetry.addData("isVisible", vuforiaResult.isVisible);
-    //if(vuforiaResult.isVisible && (Double.parseDouble(JavaUtil.formatNumber(vuforiaResult.x, 2)) >= 140)&&Double.parseDouble(JavaUtil.formatNumber(vuforiaResult.x, 2)) <= 150){
-       // shoot();
-       // while(vuforiaResult.isVisible){
-           // shooter_thing.setPower(1);
-       // }
-   // }
-    telemetry.addData("X (mm)", Double.parseDouble(JavaUtil.formatNumber(vuforiaResult.x, 2)));
-    telemetry.addData("Y (mm)", Double.parseDouble(JavaUtil.formatNumber(vuforiaResult.y, 2)));
-    telemetry.addData("Heading (degrees)", Double.parseDouble(JavaUtil.formatNumber(vuforiaResult.zAngle, 2)));
-    
-  }
-    public void driveSimple(){
-        double power = .5;
-        if(gamepad1.dpad_up){ //Forward
-            front_left_wheel.setPower(-power);
-            back_left_wheel.setPower(-power);
-            back_right_wheel.setPower(-power);
-            front_right_wheel.setPower(-power);
-        }
-        else if(gamepad1.dpad_left){ //Left
-            front_left_wheel.setPower(power);
-            back_left_wheel.setPower(-power);
-            back_right_wheel.setPower(power);
-            front_right_wheel.setPower(-power);
-        }
-        else if(gamepad1.dpad_down){ //Back
-            front_left_wheel.setPower(power);
-            back_left_wheel.setPower(power);
-            back_right_wheel.setPower(power);
-            front_right_wheel.setPower(power);
-        }
-        else if(gamepad1.dpad_right){ //Right
-            front_left_wheel.setPower(-power);
-            back_left_wheel.setPower(power);
-            back_right_wheel.setPower(-power);
-            front_right_wheel.setPower(power);
-        }
-        else if(Math.abs(gamepad1.right_stick_x) > 0){ //Rotation
-            front_left_wheel.setPower(-gamepad1.right_stick_x);
-            back_left_wheel.setPower(-gamepad1.right_stick_x);
-            back_right_wheel.setPower(gamepad1.right_stick_x);
-            front_right_wheel.setPower(gamepad1.right_stick_x);
-        }
-        else{
-            front_left_wheel.setPower(0);
-            back_left_wheel.setPower(0);
-            back_right_wheel.setPower(0);
-            front_right_wheel.setPower(0);
-        }
     }
 
     public void drive(){
